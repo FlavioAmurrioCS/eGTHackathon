2b8f8c29fa0dfdc32517411c1cbe708939782793
Flavio <FlavioAmurrioCS@gmail.com>
2017-10-20 19:37:50 -0400

    Refactoring

--- PreProcessing.py
+++ PreProcessing.py
@@ -1,21 +1,19 @@
-from googletrans import Translator as translator
+from googletrans import Translator
 
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 
 from PyDictionary import PyDictionary as dict
 
+from nltk.stem import PorterStemmer
 
-
-
-
-#################### Translation ###################
-# translator = Translator()
+############################### Translation ###############################
+translator = Translator()
 
 def translate(sentence):
     return (translator.translate(sentence)).text
 
-#################### StopWords ###################    
+############################### StopWords ###############################
 stop_words = set(stopwords.words('english'))
 
 def stopRemToken(word_tokens):
@@ -28,15 +26,18 @@ def stopRemSent(sentence):
     retStr = "".join(" " + str(x) for x in tokens)
     return retStr
 
-################### Synonyms ################################
+############################### Synonyms ###############################
 def synom(word):
     return dict.synonym(word)
 
-##################################################
-from nltk.stem import PorterStemmer
-from nltk.tokenize import sent_tokenize, word_tokenize
-
+############################### Stemmer ###############################
 ps = PorterStemmer()
+
+def stemToken(words):
+    ret = []
+    for word in words:
+        ret.append(ps.stem(word))
+    return ret
 
 def stemSent(sentence):
     words = word_tokenize(sentence)
@@ -44,11 +45,8 @@ def stemSent(sentence):
     retStr = "".join(" " + str(x) for x in stem_token)
     return retStr
 
-def stemToken(words):
-    ret = []
-    for word in words:
-        ret.append(ps.stem(word))
-    return ret
+def remSp(word):
+    return ''.join(e for e in word if e.isalpha())
 
 def remSpec(words_token):
     ret = []
@@ -59,17 +57,26 @@ def remSpec(words_token):
             ret.append(temp)            
     return ret
 
-def remSp(word):
-    return ''.join(e for e in word if e.isalpha())
+############################### Combined Tools ###############################
 
+comment = "This book is such a life saver.  It has been so helpful to be able to go back to track trends, answer pediatrician questions, or communicate with each other when you are up at different times of the night with a newborn.  I think it is one of those things that everyone should be required to have before they leave the hospital.  We went through all the pages of the newborn version, then moved to the infant version, and will finish up the second infant book (third total) right as our baby turns 1.  See other things that are must haves for baby at [...]"
 
-####################
 def clean(sentence):
-    sentence = translate(sentence)
+    # sentence = translate(sentence)
     sentence = str.lower(sentence)
     words = word_tokenize(sentence)
     words = stopRemToken(words)
     words = stemToken(words)
     words = remSpec(words)
-    return words
+    line = "".join(" " + str(x) for x in words)
+    return line
 
+def readfile(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        with open(filename + "-output", 'w') as w:
+            for line in lines:
+                w.write(clean(line) + "\n")
+
+
+readfile('file.txt')
